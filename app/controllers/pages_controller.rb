18a7540633074f9d47b11c58a7339866b81b74e5
Cecile Rougnaux <cecile_rougnaux@hotmail.com>
2020-03-04 22:31:16 +0100

    contact form set- no layout yet

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,22 @@
 class PagesController < ApplicationController
   def home
+     @contact = Pages.new(params[:pages])
+  end
+
+  def create
+    @contact = Pages.new(params[:pages])
+    @contact.request = request
+    respond_to do |format|
+      if @contact.deliver
+        # re-initialize Pages object for cleared form
+        puts 'hello world'
+        @contact = Pages.new
+        format.html { render 'home' }
+        format.js   { flash.now[:success] = @message = "Thank you for your message. I'll get back to you soon!" }
+      else
+        format.html { render 'home' }
+        format.js   { flash.now[:error] = @message = "Message did not send." }
+      end
+    end
   end
 end
