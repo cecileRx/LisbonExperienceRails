4d96dc355976cb8ed43f88768cf9c9a14b4ab266
Cecile Rougnaux <cecile_rougnaux@hotmail.com>
2020-03-24 16:53:41 +0000

    template title and nabvar modified

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,19 +15,22 @@ User.destroy_all
 puts 'creating tours'
 
 tour1 = Tour.new(
-  name: 'Tour of the day',
+  name: 'Tour one – The Mediterranean Civilizations',
   description: 'lorem ipsum Aute elit duis qui irure . Fugiat et consequat non dolor cillum incididunt dolor eu aliquip excepteur nisi nostrud dolor veniam ad',
-  full_information: 'lorem ipsum Aute elit duis qui irure . Fugiat et consequat non dolor cillum incididunt dolor eu aliquip excepteur nisi nostrud dolor veniam adlorem ipsum Aute elit duis qui irure . Fugiat et consequat non dolor cillum incididunt dolor eu aliquip excepteur nisi nostrud dolor veniam ad'
+  full_information: 'We’ll start by picking-up a funicular, a popular public transport, to reach the top of Santana Hill and there I’ll share with you a story about a Portuguese Princess who was also Queen of England. This is also the trigger to tell you a brief story about the Portuguese tiles and on our walk we’ll see the most stunning tile facades of Lisbon. We will then get to Mouraria neighbourhood, the birthplace of Fado, and also an area with an impressive cultural dynamic. We’ll have a look at Lisbon’s street art while we climb Graça’s hill. Once at the top you’ll be rewarded with a memorable view while resting you legs a bit and having a refreshing drink. At the next viewpoint in Alfama’s district we’ll meet one of Lisbon’s symbols, Saint Vincent monastery, which explains a lot of our cultural influences. We’ll end this tour at Comercio Square the most important landmark with right in front of Tagus river.
+Meeting point in front of Hard Rock Café Lisboa'
   )
 
-file = URI.open("https://res.cloudinary.com/cecilerxdm71vcz1e/image/upload/v1584458546/LisbonExperience/Mod_Lisboa_M0367_oyshdu.jpg")
+file = URI.open("https://res.cloudinary.com/cecilerxdm71vcz1e/image/upload/v1584463037/LisbonExperience/Tiles_ofa87k.jpg")
 tour1.photo.attach(io: file, filename: "some-image.jpg", content_type: 'image/jpg')
 tour1.save!
 
 tour2 = Tour.new(
-  name: 'Tour of the month',
-  description: 'lorem ipsum Aute elit duis qui irure . Fugiat et consequat non dolor cillum incididunt dolor eu aliquip excepteur nisi nostrud dolor veniam ad',
-  full_information: 'lorem ipsum Aute elit duis qui irure . Fugiat et consequat non dolor cillum incididunt dolor eu aliquip excepteur nisi nostrud dolor veniam adlorem ipsum Aute elit duis qui irure . Fugiat et consequat non dolor cillum incididunt dolor eu aliquip excepteur nisi nostrud dolor veniam ad'
+  name: 'Tour two – The Portuguese Tiles',
+  description: 'thematic tour about the tile’s history in Lisbon',
+  full_information: 'This is a thematic tour about the tile’s history in Lisbon. Tiles were used for five centuries as a decorative art being an important part of our cultural identity. Started to adorn living rooms, stairs and gardens but by the beginning of the XIX century they jumped outside to the Lisbon facades making this city an open-air museum. We will start at Casa do Alentejo an old casino which is fully decorated with moorish tiles. From there we’ll head to Bairro Alto hill where we’ll meet artwork tiles in its different kind of expressions. There it will be possible to admire outdoors tile panels as well indoor ones. We’ll meet XX century artworks till the very first tiles with the Portuguese brand from the XVI century.
+In our pathway I’ll talk about some of our most important historical moments like the big disaster of 1755 and the Jewish massacre of 1506.
+Meeting point in front of Hard Rock Café Lisboa'
   )
 file = URI.open("https://res.cloudinary.com/cecilerxdm71vcz1e/image/upload/v1584458546/LisbonExperience/Mod_Lisboa_M0367_oyshdu.jpg")
 tour2.photo.attach(io: file, filename: "some-image.jpg", content_type: 'image/jpg')
