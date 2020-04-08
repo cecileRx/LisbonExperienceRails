fd32aef81bdf70b3a06953e2c164a80ef9d9a1f6
Cecile Rougnaux <cecile_rougnaux@hotmail.com>
2020-04-08 10:27:27 +0200

    contact form in production - correction

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-end
+
 
 config.action_mailer.default_url_options = { :host => 'frozen-scrubland-52032.herokuapp.com' }
 config.action_mailer.delivery_method = :smtp
@@ -107,3 +107,5 @@ config.action_mailer.smtp_settings = {
   password:             ENV["GMAIL_PASSWORD"],
   authentication:       'plain',
   enable_starttls_auto: true  }
+
+end
