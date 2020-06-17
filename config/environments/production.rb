6eedae0db44a4f3411391e147274195d1281ea10
Cecile Rougnaux <cecile_rougnaux@hotmail.com>
2020-06-17 12:28:40 +0200

    domain change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,7 +102,7 @@ config.action_mailer.default :charset => "utf-8"
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'frozen-scrubland-52032.herokuapp.com',
+  domain:               'gmail.com',
   user_name:            ENV["GMAIL_EMAIL"],
   password:             ENV["GMAIL_PASSWORD"],
   authentication:       'plain',
